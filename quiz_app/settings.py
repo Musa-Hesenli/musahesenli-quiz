8acd8bb74632ab1509e5ff3e53cfcd659e23b048
Musa-Hesenli <musahesenli02@gmail.com>
2021-07-10 11:15:33 +0400

    df

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'quiz_app.urls'
