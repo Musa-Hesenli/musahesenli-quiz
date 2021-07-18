22d21cc8da7b7c06eefa33180811daaed2913217
Musa-Hesenli <musahesenli02@gmail.com>
2021-07-18 12:08:58 +0400

    p

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
+
+@admin.register(models.Questions)
+class QuestionAdminModel(admin.ModelAdmin):
+    list_filter = ('package',)
+    list_display = ('package', 'question_name')
+    list_display_links = ["package"]
+    class Meta:
+        model = models.Questions
+
+@admin.register(models.Answers)
+class AnswersModelAdmin(admin.ModelAdmin):
+    list_filter = ["question"]
+    list_display = ["question", "text", "is_answer"]
+
 admin.site.register(models.Category)
-admin.site.register(models.Questions)
-admin.site.register(models.Answers)
 admin.site.register(models.QuestionPackage)
 admin.site.register(models.CustomerCreatedQuiz)
 admin.site.register(models.Rank)
